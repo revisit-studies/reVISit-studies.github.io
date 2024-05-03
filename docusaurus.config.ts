98431f15b98bb3fa6f00a691a5e866c952f56789
Brian Bollen <briancbollen@gmail.com>
2024-05-03 16:01:38 -0600

    Changed image switcher to use custom css rather than colorMode context

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,31 +84,31 @@ const config: Config = {
           html:` 
             <div class="footer-wrapper">
               <a target="_blank" href="https://vdl.sci.utah.edu/">
-                <img src="/img/logos/vdl-logo-light.svg" class='logo light-theme'/>
-                <img src="/img/logos/vdl-logo-dark.svg" class='logo dark-theme'/>
+                <img src="/img/logos/vdl-logo-light.svg" class='logo light-theme-display-component'/>
+                <img src="/img/logos/vdl-logo-dark.svg" class='logo dark-theme-display-component'/>
               </a>
               <a target="_blank" href="https://www.sci.utah.edu/">
-                <img src="/img/logos/sci-logo-light.svg" class='logo light-theme'/>
-                <img src="/img/logos/sci-logo-dark.svg" class='logo dark-theme'/>
+                <img src="/img/logos/sci-logo-light.svg" class='logo light-theme-display-component'/>
+                <img src="/img/logos/sci-logo-dark.svg" class='logo dark-theme-display-component'/>
               </a>
               <a target="_blank" href="https://www.cs.utah.edu/">
-                <img src="/img/logos/ULogo-light.svg" class='logo light-theme'/>
-                <img src="/img/logos/ULogo-dark.svg" class='logo dark-theme'/>
+                <img src="/img/logos/ULogo-light.svg" class='logo light-theme-display-component'/>
+                <img src="/img/logos/ULogo-dark.svg" class='logo dark-theme-display-component'/>
               </a>
               <a target="_blank" href="http://web.cs.wpi.edu/~ltharrison/">
-                <img src="/img/logos/view-logo-light.svg" class='logo light-theme'/>
-                <img src="/img/logos/view-logo-dark.svg" class='logo dark-theme'/>
+                <img src="/img/logos/view-logo-light.svg" class='logo light-theme-display-component'/>
+                <img src="/img/logos/view-logo-dark.svg" class='logo dark-theme-display-component'/>
               </a>
               <a target="_blank" href="https://wpi.edu/">
                 <img src="/img/logos/wpi.png" class='logo'/>
               </a>
               <a target="_blank" href="https://www.cs.toronto.edu/~cnobre/">
-                <img src="/img/logos/HIVE_logo.png" class='logo light-theme'/>
-                <img src="/img/logos/HIVE-logo-dark.svg" class='logo dark-theme'/>
+                <img src="/img/logos/HIVE_logo.png" class='logo light-theme-display-component'/>
+                <img src="/img/logos/HIVE-logo-dark.svg" class='logo dark-theme-display-component'/>
               </a>
               <a target="_blank" href="https://www.cs.toronto.edu/">
-                <img src="/img/logos/UofTLogo.png" class='logo light-theme'/>
-                <img src="/img/logos/UofT-logo-dark.svg" class='logo dark-theme'/>
+                <img src="/img/logos/UofTLogo.png" class='logo light-theme-display-component'/>
+                <img src="/img/logos/UofT-logo-dark.svg" class='logo dark-theme-display-component'/>
               </a>
               <a target="_blank" href="https://www.nsf.gov/">
                 <img src="/img/logos/nsf.png" class='logo'/>
