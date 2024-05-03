b76356c3566961eee5a564097239834508c8dbcc
Jack Wilburn <jackwilburn@tutanota.com>
2024-05-03 10:49:47 -0600

    Remove some more references to blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,13 +43,6 @@ const config: Config = {
           },
           // includeCurrentVersion: false
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -78,7 +71,6 @@ const config: Config = {
           label: 'Documentation',
         },
         {to: '/faq', label: 'FAQ', position: 'left'},
-        // {to: '/blog', label: 'Blog', position: 'left'},
         {
           type: 'docsVersionDropdown',
           position: 'right',
