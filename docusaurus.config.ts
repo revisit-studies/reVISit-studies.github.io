0bd6045d2909e9e2a8e51c9206bc3687413cfb02
Brian Bollen <briancbollen@gmail.com>
2024-05-16 10:37:15 -0600

    Testing Search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,6 +23,7 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+  
 
   presets: [
     [
@@ -47,6 +48,38 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'CE7T3Q5S25',
+
+      // Public API key: it is safe to commit it
+      apiKey: '0e172022221452330a1cf49f25f6ebc6',
+
+      indexName: 'revisit',
+
+      // Ensures that search is based on version
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
     image: 'img/logos/revisitLogoThumbnail.png',
     colorMode: {
       respectPrefersColorScheme: true,
