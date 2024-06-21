cb9dcac749f33f43614b004f83c518e31619a99d
Brian Bollen <briancbollen@gmail.com>
2024-06-21 17:17:31 -0600

    Fixed mobile styles

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -6,8 +6,11 @@ export default function Banner(): JSX.Element {
   return (
     <section className={styles.banner}>
       <div className="container">
-        <div className={clsx('column',styles.bannerColumn)}>
-          <div className={styles.bannerTitle}>ReVISit Version 1.0.0 Is Out! &#127881; <a target="_blank" href="https://vdl.sci.utah.edu/blog/2024/06/20/revisit/">Read the blog post.</a></div>
+        <div className={styles.bannerColumn}>
+          <div className={styles.bannerTitle}>ReVISit Version 1.0.0 Is Out! &#127881;</div>
+          <div className={styles.bannerSubtitle}>
+            <a target="_blank" href="https://vdl.sci.utah.edu/blog/2024/06/20/revisit/">Read the blog post.</a>
+          </div>
         </div>
       </div>
     </section>
