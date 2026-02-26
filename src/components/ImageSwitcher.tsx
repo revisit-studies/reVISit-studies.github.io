bc2ce0eacd95b615cf517fb4a29683f352921c51
Jack Wilburn <jackwilburn@tutanota.com>
2026-02-25 21:44:12 -0700

    re-add alt text to images

--- src/components/ImageSwitcher.tsx
+++ src/components/ImageSwitcher.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 interface ImageSwitcherProps {
   lightImageSrc: string,
   darkImageSrc: string,
+  alt: string,
   style?: React.CSSProperties,
   className?: string
 }
 
 function ImageSwitcher({
-  lightImageSrc, darkImageSrc, style, className,
+  lightImageSrc, darkImageSrc, alt, style, className,
 } : ImageSwitcherProps) {
   let darkClassName = 'dark-theme-display-component';
   let lightClassName = 'light-theme-display-component';
@@ -21,11 +22,13 @@ function ImageSwitcher({
     <>
       <img
         style={style}
+        alt={alt}
         src={lightImageSrc}
         className={lightClassName}
       />
       <img
         style={style}
+        alt={alt}
         src={darkImageSrc}
         className={darkClassName}
       />
